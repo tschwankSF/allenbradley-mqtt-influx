02bf060c55f2436ddec7c171104a0f8e53f820ae
Thomas <53979638+tschwankSF@users.noreply.github.com>
2021-03-28 21:34:06 -0400

    initial commit

---
+++ ab-mqtt-basics.py
@@ -0,0 +1,18 @@
+# import AB PLC Library "pylogix". 
+# details about pylogix: https://github.com/dmroeder/pylogix 
+from pylogix import PLC
+
+# create PLC object
+ab = PLC()
+
+# set IP address of PLC
+ab.IPAddress = '10.141.111.253'
+
+# read one tag
+t = ab.Read('ASSET[1].PARTCOUNT')
+
+# print values
+print('\nTag Name: ', t.TagName, '\tTag Value: ', t.Value, '\n')
+
+# close connection to PLC
+ab.Close()
